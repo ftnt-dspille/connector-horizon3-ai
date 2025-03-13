c1308d469b2148d858d0873ddd6b1b49434d765e
Dylan Spille <dspille@fortinet.com>
2025-03-13 16:05:53 -0500

    add data sample file and remove unnecessary file

---
+++ tests/data_sample.py
@@ -0,0 +1,16 @@
+"""Rename file to data.py"""
+config = {
+    "verify_ssl": True,
+    "api_token": "add api token here",
+    "server_url": "https://api.horizon3ai.com/"
+}
+invalid_config = {
+    "api_token": "invalid-token",
+    "server_url": "https://api.horizon3ai.com",
+    "verify_ssl": True
+}
+
+connector_info = {
+    "connector_name": "horizon-ai",
+    "connector_version": "1.0.0"
+}
